26fa8f56ccf49730090bceebd06435944e47fdda
KgothatsoTheko <kgothatsotheko7@gmail.com>
2024-01-23 15:37:24 +0200

    views

--- src/app/components/view-candidates/view-candidates.component.ts
+++ src/app/components/view-candidates/view-candidates.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-view-candidates',
   templateUrl: './view-candidates.component.html',
   styleUrls: ['./view-candidates.component.scss']
 })
-export class ViewCandidatesComponent {
+export class ViewCandidatesComponent implements AfterViewInit {
+
+  mainclass: any
+  displayedColumns: string[] = ['classId', 'firstName', 'lastName', 'email', 'role'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  constructor(private sharedService: SharedService) {
+    this.mainclass = this.sharedService.get('class', 'session')
+    this.dataSource = new MatTableDataSource(this.mainclass.candidates)
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
 }
--- src/app/components/view-late-report/view-late-report.component.ts
+++ src/app/components/view-late-report/view-late-report.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ExportExcelService } from 'src/app/services/export-excel.service';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-view-late-report',
   templateUrl: './view-late-report.component.html',
-  styleUrls: ['./view-late-report.component.scss']
+  styleUrls: ['./view-late-report.component.scss'],
+  providers: [DatePipe]
 })
-export class ViewLateReportComponent {
+export class ViewLateReportComponent implements AfterViewInit {
+  currentUser: any
+  lateReports: any[] = []
+  currentClass: any
+  displayedColumns: string[] = ['reportId', 'createdBy', 'timeEstimate', 'dateCreated', 'reason', 'arrivalTime', 'updatedBy', 'changeStatus'];
+  dataSource!: MatTableDataSource<any>;
+
+  constructor(private sharedService: SharedService, private datePipe: DatePipe, private excelService: ExportExcelService) {
+    this.currentClass = this.sharedService.get('class', 'session')
+    this.currentUser = this.sharedService.get('user', 'session')
+    this.updateReports()
+  }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  updateReports() {
+    const allLateReports = this.sharedService.get('lateReports', 'local') || []
+    this.lateReports = allLateReports.filter((reports:any) => reports.classId === this.currentClass.classID)
+
+    this.dataSource = new MatTableDataSource(this.lateReports);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  statusUpdate(status: string, reportId: string): void {
+    this.lateReports.forEach((report: any, indx: number) => {
+      if (report.reportId === reportId) {
+        this.lateReports[indx]['status'] = status;
+        this.lateReports[indx]['updatedBy'] = `${this.currentUser.firstName} ${this.currentUser.lastName}`;
+        localStorage.setItem('lateReports', JSON.stringify(this.lateReports));
+      }
+    });
+
+    this.updateReports();
+  }
+
+  exportToExcel(): void {
+    const fileName = `late-report_${this.datePipe.transform(new Date(), 'dd-MM-YYYY')}.xlsx`;
+    this.excelService.exportToExcel(fileName, this.dataSource.filteredData);
+  }
 
 }
