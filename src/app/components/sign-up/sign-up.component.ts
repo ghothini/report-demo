e9ec09c080c7a01ff0fbc82da0e5112f22a823ac
unknown <thapeloghothini@gmail.com>
2024-01-23 15:01:55 +0200

    Update

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,7 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  classes: any = [];
+  roles: string[] = ['candidate','facilitator']
   registerFormData: any = {
+    firstName: '',
+    lastName: '',
+    role: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+  constructor(private router: Router, private snackbar: MatSnackBar){
     
+  }
+
+  submit(){
+
   }
 }
