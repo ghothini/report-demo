5cd3ae45a069e2ef2c7b1f771eed625e8d26c1dc
Simphiwe Makhathini <gsimphiwe212@gmail.com>
2024-01-23 15:12:23 +0200

    'home'

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+ user: any;
+ class: any;
+ menuItems: any = []
+ activeLink: number = 0
+
+ constructor(private router: Router) {
+
+  this.user = {name: 'Simphiwe Nene', role: 'facilitator'}
+  this.class = {className: 'Maths'}
+
+
+  if(this.user === 'candidate') {
+    this.menuItems = [{name: 'View Late Report', route: 'candidate/late-report'}]
+  }else {
+    this.menuItems = [{name: 'View Candidate', route: 'facilitator/late-report'},
+    {name: 'View Late Comers', route: 'facilitator/view-late-report'}]
+  }
+
+ this.listenToRouteChange()
+
+ }
+
+ listenToRouteChange() {
+  this.router.events.subscribe(event => {
+    if(event instanceof NavigationEnd) {
+      this.menuItems.forEach((item: any, indx: number) => {
+        if(item.route === event.url) {
+          this.activeLink = indx
+        }
+      })
+    }
+  })
+ }
+
+ logOut(): void {
+  sessionStorage.clear()
+  this.router.navigate(['sign-in'])
+ }
 
 }
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
+  user: any;
+  classes: any;
 
+  constructor() {}
 }
